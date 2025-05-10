25d61298187ebb1ffff2c402aba1e4bed7de64a6
kenrio <kenken7272721@gmail.com>
2025-05-10 17:21:18 +0900

    Minor changes.

--- src/render/hit_sphere.c
+++ src/render/hit_sphere.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 21:58:50 by keishii           #+#    #+#             */
-/*   Updated: 2025/05/09 18:31:50 by keishii          ###   ########.fr       */
+/*   Updated: 2025/05/10 17:13:57 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 
 bool	hit_sphere(t_ray r, t_vec3 center, double radius)
 {
-    t_vec3 oc = v_sub(r.orig, center);
-    double a = v_dot(r.dir, r.dir);
-    double b = 2 * v_dot(oc, r.dir);
-    double c = v_dot(oc, oc) - radius*radius;
-    double disc = b*b - 4*a*c;
-    if (disc < 0) return false;
-    double t = (-b - sqrt(disc)) / (2*a);
-    if (t < 1e-4) t = (-b + sqrt(disc)) / (2*a);
-    if (t < 1e-4) return false;
-    return (true);
+	t_vec3	oc;
+	double	a;
+	double	b;
+	double	c;
+	double	discriminant;
+	double	t;
+
+	oc = v_sub(r.orig, center);
+	a = v_dot(r.dir, r.dir);
+	b = 2 * v_dot(oc, r.dir);
+	c = v_dot(oc, oc) - radius * radius;
+	discriminant = b * b - 4 * a * c;
+	if (discriminant < 0)
+		return (false);
+	t = (-b - sqrt(discriminant)) / (2 * a);
+	if (t < 1e-4)
+	{
+		t = (-b + sqrt(discriminant)) / (2 * a);
+		if (t < 1e-4)
+			return (false);
+	}
+	return (true);
 }
