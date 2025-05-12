43e199477d5b0aabb8eb460994e1e2566ac4910c
kenrio <kenken7272721@gmail.com>
2025-05-12 17:08:30 +0900

    Modify if statement.

--- src/render/intersect_sphere.c
+++ src/render/intersect_sphere.c
@@ -6,7 +6,7 @@
 /*   By: keishii <keishii@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 21:58:50 by keishii           #+#    #+#             */
-/*   Updated: 2025/05/11 14:28:28 by keishii          ###   ########.fr       */
+/*   Updated: 2025/05/12 17:07:30 by keishii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,7 @@ bool	intersect_sphere(t_ray r, t_pos3 center, double radius)
 		return (false);
 	if (solve_quad_eq(&quad) == false)
 		return (false);
-	if (quad.t1 < 1e-4)
-		if (quad.t2 < 1e-4)
-			return (false);
+	if ((quad.t1 < 1e-4) && (quad.t2 < 1e-4))
+		return (false);
 	return (true);
 }
