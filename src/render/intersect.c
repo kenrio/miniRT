b6b4580dfeaf9bc519076201c9a45841394ea792
Anya-Stella <wingstonetone9.8@gmail.com>
2025-05-15 11:12:12 +0900

    pl作成まで

--- src/render/intersect.c
+++ src/render/intersect.c
@@ -6,7 +6,7 @@
 /*   By: anya_stella <anya_stella@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 21:58:50 by keishii           #+#    #+#             */
-/*   Updated: 2025/05/15 08:50:19 by anya_stella      ###   ########.fr       */
+/*   Updated: 2025/05/15 11:07:57 by anya_stella      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,28 +43,20 @@ bool	intersect_sphere(t_ray *r, t_obj *o, t_hit *rec, double t_max)
 // 平面と交わるか
 bool	intersect_plane(t_ray *r, t_obj *o, t_hit *rec, double t_max)
 {
-	t_vec3		oc;
-	t_quad_eq	quad;
-	double		t;
+	t_plane	pl;
+	double	t;
 
-	oc = pos_sub(r->origin, o->data.pl.pos);
-	quad.a = vec_dot(r->direction, r->direction);
-	quad.b = 2.0 * vec_dot(oc, r->direction);
-	quad.c = vec_dot(oc, oc) - pow(10 * 0.5, 2);
-	if (calc_quad_discriminant(&quad) < 0.0)
-		return (false);
-	solve_quad_eq(&quad);
-	t = quad.t1;
-	if (t < T_MIN || t_max < t)
-	{
-		t = quad.t2;
-		if (t < T_MIN || t_max < t)
-			return (false);
-	}
+	pl = o->data.pl;
+	t = vec_dot(pos_sub(pl.pos, r->origin), pl.vec) / vec_dot(r->direction, pl.vec);
+	if (t < T_MIN ||  t_max < t)
+        return (false);
 	rec->t = t;
 	rec->pos = ray_at(r, t);
-	rec->n = vec_normalize(pos_sub(rec->pos, o->data.pl.pos));
 	rec->rgb = o->data.pl.rgb;
+	if (vec_dot(r->direction, pl.vec) > 0)
+		rec->n = vec_scale(pl.vec, -1.0);
+	else
+		rec->n = pl.vec;
 	return (true);
 }
 
