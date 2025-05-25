117e1e089ba914ff3124a4c6c71ada19df1c701d
Anya-Stella <wingstonetone9.8@gmail.com>
2025-05-25 20:53:48 +0900

    render_high_on

--- src/render/render_scene.c
+++ src/render/render_scene.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render_scene.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anya_stella <anya_stella@student.42.fr>    +#+  +:+       +#+        */
+/*   By: tishihar <wingstonetone9.8@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2025/05/24 01:06:27 by anya_stella      ###   ########.fr       */
+/*   Updated: 2025/05/25 17:45:05 by tishihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,35 @@
 
 // this fuctiuon emit a ray of light,
 // if ray hit something, update "t_hit rec".
-void	render_scene(t_info *info)
-{
-	int				x;
-	int				y;
-	t_ray			ray;
-	unsigned int	color;
-	t_hit			rec;
+// void	render_scene(t_info *info)
+// {
+// 	int				x;
+// 	int				y;
+// 	t_ray			ray;
+// 	unsigned int	color;
+// 	t_hit			rec;
 
-	y = 0;
-	while (y < WIN_H)
-	{
-		x = 0;
-		while (x < WIN_W)
-		{
-			ray = make_ray(
-					&info->cam,
-					((double)x / (WIN_W - 1)),
-					(1.0 - (double)y / (WIN_H - 1)));
-			if (hit_scene(&ray, info->objs, &rec) == true)
-			{
-				color
-					= rgb_to_uint(
-						calculate_lighting(info, &rec, &ray, 0));
-			}
-			else
-				color = BG_COLOR;
-			put_pixel(x, y, color, &info->mlx.img);
-			x++;
-		}
-		y++;
-	}
-}
+// 	y = 0;
+// 	while (y < WIN_H)
+// 	{
+// 		x = 0;
+// 		while (x < WIN_W)
+// 		{
+// 			ray = make_ray(
+// 					&info->cam,
+// 					((double)x / (WIN_W - 1)),
+// 					(1.0 - (double)y / (WIN_H - 1)));
+// 			if (hit_scene(&ray, info->objs, &rec) == true)
+// 			{
+// 				color
+// 					= rgb_to_uint(
+// 						calculate_lighting(info, &rec, &ray, 0));
+// 			}
+// 			else
+// 				color = BG_COLOR;
+// 			put_pixel(x, y, color, &info->mlx.img);
+// 			x++;
+// 		}
+// 		y++;
+// 	}
+// }
